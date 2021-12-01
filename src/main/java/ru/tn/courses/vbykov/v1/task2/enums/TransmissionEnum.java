3cfa528840f70197491e4e8bb3870a3b4fc5540e
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2021-12-01 18:59:10 +0300

    Create TransmissionEnum.java

---
+++ src/main/java/ru/tn/courses/vbykov/v1/task2/enums/TransmissionEnum.java
@@ -0,0 +1,17 @@
+package ru.tn.courses.vbykov.v1.task2.enums;
+
+public enum TransmissionEnum {
+
+    MANUAL("механическая"), AUTOMATIC("автоматическая");
+
+    private final String value;
+
+    TransmissionEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
