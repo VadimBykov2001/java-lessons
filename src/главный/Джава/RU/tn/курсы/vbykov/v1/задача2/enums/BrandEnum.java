d5fc0c0efb444ccb2016b47e812cbc5e4ee65dd2
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2021-12-01 17:04:38 +0300

    Create BrandEnum.java

---
+++ src/главный/Джава/RU/tn/курсы/vbykov/v1/задача2/enums/BrandEnum.java
@@ -0,0 +1,16 @@
+package ru.tn.courses.vbogatyrev.v1.task2.enums;
+
+public enum BrandEnum {
+    KIA("киа"), Lada("лада"), Toyota("тойота");
+
+    private final String value;
+
+    BrandEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
