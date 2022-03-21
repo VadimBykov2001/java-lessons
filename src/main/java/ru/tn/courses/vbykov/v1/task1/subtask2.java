223966e076999e9417e0d684f6182f65c41317ce
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 15:37:33 +0300

    Update subtask2.java

--- src/main/java/ru/tn/courses/vbykov/v1/task1/subtask2.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task1/subtask2.java
@@ -1,33 +1,27 @@
 package ru.tn.courses.vbykov.v1.task1;
-
 import java.util.Scanner;
 
 public class subtask2 {
     /**
-     * У прилавка магазина выстроилась очередь из n покупателей. Время обслуживания i-того покупателя равно tj (i = 1, …, n).
-     * Определить время Ci пребывания i-гo покупателя в очереди.
+     * У прилавка магазина выстроилась очередь из n покупателей. Время обслуживания i-того покупателя равно tj (i = 1, …, n). Определить время Ci пребывания i-гo покупателя в очереди.
      **/
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите длину очереди n:");
-
         int n = scanner.nextInt();
         double[] t = new double[n];
         double[] c = new double[n];
-
         System.out.println("Введите время обслуживания i покупателя:");
-
         for (int i = 0; i < n; i++) {
             t[i] = scanner.nextDouble();
             if (i == 0) {
                 c[i] = 0;
-            } else {
+            } 
+            else {
                 c[i] = c[i - 1] + t[i - 1];
             }
         }
-
         System.out.println("Введите пребывания i покупателя в очереди:");
         for (int i = 0; i < n; i++) {
             System.out.println(i + 1 + " покупатель : " + c[i] + " единиц времени в очереди");
