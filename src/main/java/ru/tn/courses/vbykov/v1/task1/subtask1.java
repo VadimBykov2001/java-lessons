a4b8e50650ac13ac24f6571655f935a9d73d4e20
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 15:22:20 +0300

    Update subtask1.java

--- src/main/java/ru/tn/courses/vbykov/v1/task1/subtask1.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task1/subtask1.java
@@ -1,31 +1,24 @@
 package ru.tn.courses.vbykov.v1.task1;
-
 import java.util.Scanner;
 
 public class subtask1 {
     /**
-     * Дан массив натуральных чисел. Найти сумму элементов, кратных данному K.
+     * (Задание 1 варианта) Дан массив натуральных чисел. Найти сумму элементов, кратных данному k.
      **/
 
     public static void main(String[] args) {
-
-        int[] array = new int[10];
+        int[] array = new int[5];
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("введите k:");
-
         int k = scanner.nextInt();
         int sum = 0;
-
-        System.out.println("введите массив размера 10:");
-
+        System.out.println("введите массив размера 5:");
         for (int i : array) {
             array[i] = scanner.nextInt();
             if (array[i] % k == 0) {
                 sum += array[i];
             }
         }
-
-        System.out.println("сумма элементов массива кратных числу k = " + sum);
+        System.out.println("Сумма элементов массива кратных числу k = " + sum);
     }
 }
