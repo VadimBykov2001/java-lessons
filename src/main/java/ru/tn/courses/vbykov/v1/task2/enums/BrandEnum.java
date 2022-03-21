337fc6bfbcf22dc9c158ca75131ef5e55e5ad74f
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 16:30:48 +0300

    Update BrandEnum.java

--- src/main/java/ru/tn/courses/vbykov/v1/task2/enums/BrandEnum.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task2/enums/BrandEnum.java
@@ -1,16 +1,12 @@
 package ru.tn.courses.vbykov.v1.task2.enums;
 
 public enum BrandEnum {
-    KIA("киа"), Lada("лада"), BMW("бмв");
-
+    Samsung("Самсунг"), Apple("Эппел"), Xiaomi("Ксяоми");
     private final String value;
-
     BrandEnum(String value) {
         this.value = value;
     }
-
     public String getValue() {
         return value;
     }
-
 }
