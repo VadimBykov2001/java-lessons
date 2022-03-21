d48c226b0f407dae8de496ba3a10e39ee770b194
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 15:45:12 +0300

    Update subtask3.java

--- src/main/java/ru/tn/courses/vbykov/v1/task1/subtask3.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task1/subtask3.java
@@ -1,33 +1,26 @@
 package ru.tn.courses.vbykov.v1.task1;
-
 import java.util.Scanner;
 
 public class subtask3 {
     /**
-     * Даны две последовательности a1 ≤ a2 ≤ ... ≤ аn и b1 ≤ b2 ≤ ... ≤ bn. Образовать из них новую последовательность
-     * чисел так, чтобы она тоже была неубывающей (дополнительный массив не использовать).
+     * (Задание 1 варианта) Даны две последовательности a1 ≤ a2 ≤ ... ≤ аn и b1 ≤ b2 ≤ ... ≤ bn. Образовать из них новую последовательность чисел так, чтобы она тоже была неубывающей (дополнительный массив не использовать).
      **/
 
     public static void main(String[] args) {
         int[] a, b;
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите длину последовательностей n:");
-
         int n = scanner.nextInt();
         a = new int[n];
         b = new int[n];
-
         System.out.println("Введите последовательность a:");
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-
         System.out.println("Введите последовательность b:");
         for (int i = 0; i < n; i++) {
             b[i] = scanner.nextInt();
         }
-
         System.out.println("Новая последовательность без нового массива:");
         for (int i = 0, j = 0; i < n && j < n; j++) {
             while (i < n && a[i] <= b[j]) {
