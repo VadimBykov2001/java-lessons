8e230b3f61e9e31fa08a7de53a006cd92f89ad7c
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 16:49:14 +0300

    Update OrderDescriptionEnum.java

--- src/main/java/ru/tn/courses/vbykov/v1/task2/enums/OrderDescriptionEnum.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task2/enums/OrderDescriptionEnum.java
@@ -1,14 +1,11 @@
 package ru.tn.courses.vbykov.v1.task2.enums;
 
 public enum OrderDescriptionEnum {
-    CREATE("Создание заказа"), RETURN("возврат продукта"), MODIF("изменение заказа"), BUY("произведена покупка");
-
+    CREATE("Создание заказа"), RETURN("Возврат продукта"), MODIF("Изменение заказа"), BUY("Произведение покупки");
     private final String value;
-
     OrderDescriptionEnum(String value) {
         this.value = value;
     }
-
     public String getValue() {
         return value;
     }
