2e85cb758d5d2cc4f6299267daa5a2c0f57968b5
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 17:26:00 +0300

    Update CustomerRepository.java

--- src/main/java/ru/tn/courses/vbykov/v1/task2/repositories/CustomerRepository.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task2/repositories/CustomerRepository.java
@@ -1,27 +1,21 @@
 package ru.tn.courses.vbykov.v1.task2.repositories;
-
 import ru.tn.courses.vbykov.v1.task2.models.Customer;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerRepository extends RepositoryImpl<Customer> {
-
     private static CustomerRepository instance;
-
     public static CustomerRepository getInstance() {
         if (instance == null) {
             instance = new CustomerRepository();
         }
         return instance;
     }
-
     public CustomerRepository() {
         super(new ArrayList<>());
-        save(new Customer(getSize(), "Алексей", 10000.0));
-        save(new Customer(getSize(), "Константин", 100.0));
+        save(new Customer(getSize(), "Анастасия", 10000.0));
+        save(new Customer(getSize(), "Григорий", 100.0));
     }
-
     public Customer findByName(String name) throws Exception {
         List<Customer> allCustomer = findAll();
         for (Customer customer : allCustomer) {
