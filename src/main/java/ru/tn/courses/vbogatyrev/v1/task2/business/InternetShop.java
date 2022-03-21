d8196937f26f80ddecf36dc3ba4f34c93325c60a
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 16:19:04 +0300

    Update InternetShop.java

--- src/main/java/ru/tn/courses/vbogatyrev/v1/task2/business/InternetShop.java
+++ src/main/java/ru/tn/courses/vbogatyrev/v1/task2/business/InternetShop.java
@@ -1,12 +1,9 @@
 package ru.tn.courses.vbogatyrev.v1.task2.business;
-
 import ru.tn.courses.vbogatyrev.v1.task2.models.Order;
 import ru.tn.courses.vbogatyrev.v1.task2.models.Product;
-
 import java.util.List;
 
 public interface InternetShop {
-
     Order createOrder(Integer productId, String customer) throws Exception;
     Order payment(Integer orderId) throws Exception;
     Order returnProduct(Integer orderId) throws Exception;
