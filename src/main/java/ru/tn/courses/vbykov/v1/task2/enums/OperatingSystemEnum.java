ced1495da6b46447f13583c082723d84b8075e3b
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 17:06:38 +0300

    Update OperatingSystemEnum.java

--- src/main/java/ru/tn/courses/vbykov/v1/task2/enums/OperatingSystemEnum.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task2/enums/OperatingSystemEnum.java
@@ -3,7 +3,7 @@ package ru.tn.courses.vbykov.v1.task2.enums;
 public enum OperatingSystemEnum {
     Android("Андроид"), Ubuntu Touch("Убунту Тоуч"), SIRIN OS("СИРИН ОС");
     private final String value;
-    DriveEnum(String value) {
+    OperatingSystemEnum(String value) {
         this.value = value;
     }
     public String getValue() {
