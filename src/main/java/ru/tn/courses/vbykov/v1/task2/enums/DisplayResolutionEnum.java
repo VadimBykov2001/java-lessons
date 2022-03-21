906da84aeadd456e42351c8ab3568013e42b7a82
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 17:06:00 +0300

    Update DisplayResolutionEnum.java

--- src/main/java/ru/tn/courses/vbykov/v1/task2/enums/DisplayResolutionEnum.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task2/enums/DisplayResolutionEnum.java
@@ -3,7 +3,7 @@ package ru.tn.courses.vbykov.v1.task2.enums;
 public enum DisplayResolutionEnum {
     	STANDART ("1080×1920"), PLUS("1440 x 2960");
     private final String value;
-    TransmissionEnum(String value) {
+    DisplayResolutionEnum(String value) {
         this.value = value;
     }
     public String getValue() {
