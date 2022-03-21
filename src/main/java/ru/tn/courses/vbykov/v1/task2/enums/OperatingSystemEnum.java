b7489ce82f9c60ba2c5926ad88500b91cdabdac3
VadimBykov2001 <95289088+VadimBykov2001@users.noreply.github.com>
2022-03-21 16:44:02 +0300

    Update and rename DriveEnum.java to OperatingSystemEnum.java

--- src/main/java/ru/tn/courses/vbykov/v1/task2/enums/OperatingSystemEnum.java
+++ src/main/java/ru/tn/courses/vbykov/v1/task2/enums/OperatingSystemEnum.java
@@ -1,14 +1,11 @@
 package ru.tn.courses.vbykov.v1.task2.enums;
 
-public enum DriveEnum {
-    FULL("полный привод"), FRONT("передний привод"), REAR("задний привод");
-
+public enum OperatingSystemEnum {
+    Android("Андроид"), Ubuntu Touch("Убунту Тоуч"), SIRIN OS("СИРИН ОС");
     private final String value;
-
     DriveEnum(String value) {
         this.value = value;
     }
-
     public String getValue() {
         return value;
     }
